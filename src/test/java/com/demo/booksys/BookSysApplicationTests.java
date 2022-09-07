253ea1238706f22d27dea786675ecdace069af64
chanyt <chanyt166@163.com>
2022-09-07 13:06:09 +0800

    第一次初始化init

---
+++ src/test/java/com/demo/booksys/BookSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.booksys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
